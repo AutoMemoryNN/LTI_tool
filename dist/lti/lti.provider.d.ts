1e2530e5ba9715c4d777a55060186030f0132773
Juan Pablo Camargo Teherán <juanpablocamargo91@gmail.com>
2025-07-15 20:49:21 -0500

    fix: remove dist/ from repository

--- dist/lti/lti.provider.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ConfigService } from '@nestjs/config';
-export declare function createLtiProvider(config: ConfigService): void;
-export declare function setupLti(config: ConfigService): Promise<Express.Application>;
